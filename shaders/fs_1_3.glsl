dcd03706a6e3b1835913ef12fa1b13d412938f53
KesaubeEire <760093121@qq.com>
2018-12-26 18:12:21 +0800

    准备把C++14 改成 C++17

--- shaders/fs_1_3.glsl
+++ shaders/fs_1_3.glsl
@@ -3,12 +3,12 @@ out vec4 FragColor;
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexCoords;
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float shininess;
+    sampler2D diffuse;
+    vec3      specular;
+    float     shininess;
 };
 uniform Material material;
 
@@ -26,14 +26,14 @@ uniform vec3 viewPos;
 
 void main()
 {
-    // 环境光
-    vec3 ambient = light.ambient  * material.ambient;
+    // 环境光 : 用漫反射贴图
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
-    // 漫反射
+    // 漫反射 : 用漫反射贴图
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
     // 镜面光
     vec3 viewDir = normalize(viewPos - FragPos);
--- shaders/vs_1_2.glsl
+++ shaders/vs_1_2.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
+out vec2 TexCoords;
 out vec3 Normal;
 out vec3 FragPos;
 
@@ -14,7 +16,8 @@ void main()
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     FragPos = vec3(model * vec4(aPos, 1.0));
 
-    // Normal = aNormal;
     // 防止aNormal因为非对称变化丢失发现正确方向 : 法线矩阵是变换矩阵的逆矩阵的转置矩阵
     Normal = mat3(transpose(inverse(model))) * aNormal;
+
+    TexCoords = aTexCoords;
 }
